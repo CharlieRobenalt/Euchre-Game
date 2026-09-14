8ab8818dd3d0e7b7c649529bc9602dd473ebe093
AsariConnor16 <asariconnor@gmail.com>
2026-09-13 20:56:08 -0400

    Added method skeletons for game preparation

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#test
+def main():
+    print("Let's play Euchre!")
+
+def resetDeck():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = [9, 10, 11, 12, 13, 14]
+    deck = [(suit, rank) for suit in suits for rank in ranks]
+    return deck
+
+def dealCards(deck):
+    #deal each player 5 cards reserve the final 4 cards for the kitty
+    pass
